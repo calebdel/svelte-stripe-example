7a506632b652cea682ec6afd974c13a54f824307
Caleb Del Begio <delbegio@gmail.com>
2020-06-11 08:59:23 -0700

    fix

--- functions/stripe-charge/stripe-charge.js
+++ functions/stripe-charge/stripe-charge.js
@@ -52,7 +52,7 @@ exports.handler = function (event, context, callback) {
       },
       coupon: data.coupon.id,
       metadata: {
-        referral: data.referral.id,
+        referral: data.referral,
       },
     })
 
