848efa4d598f714bca43cfa09412b90382fe5ae7
Caleb Del Begio <delbegio@gmail.com>
2020-06-11 08:11:49 -0700

    s

--- functions/stripe-charge/stripe-charge.js
+++ functions/stripe-charge/stripe-charge.js
@@ -20,7 +20,8 @@ exports.handler = function (event, context, callback) {
 
   //-- Parse the body contents into an object.
   const data = JSON.parse(event.body);
-
+  const ref = JSON.parse(data.referral);
+  debugger;
   //-- Make sure we have all required data. Otherwise, escape.
   if (!data.email || !data.payment_method) {
     console.error("Required information is missing.");
@@ -55,7 +56,7 @@ exports.handler = function (event, context, callback) {
       },
       coupon: data.coupon.id,
       metadata: {
-        referral: data.referral,
+        referral: ref,
       },
     })
 
